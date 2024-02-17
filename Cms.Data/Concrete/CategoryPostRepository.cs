49d977cfc5ef3aa1c2891385ca6f34fc09d86e3c
klcuur <klcugur@outlook.com.tr>
2024-02-17 22:17:04 +0300

    categorypost guncellendi

--- Cms.Data/Concrete/CategoryPostRepository.cs
+++ Cms.Data/Concrete/CategoryPostRepository.cs
@@ -11,21 +11,21 @@ using System.Threading.Tasks;
 
 namespace Cms.Data.Concrete
 {
-	public class CategoryPostRepository : Repository<CategoryPost, AppDbContext>, IDepartmentsPostsRepository
+	public class CategoryPostRepository : Repository<CategoryPost, AppDbContext>, ICategoryPostRepository
 	{
 		private readonly AppDbContext _context;
 
-		public async Task<List<CategoryPost>> GetAllDepartmentPostByIncludeAsync()
+		public async Task<List<CategoryPost>> GetAllCategoryPostByIncludeAsync()
 		{
 			return await _context.CategoryPosts.Include(d => d.Category).Include(d => d.Post).ThenInclude(d => d.PostComments).ThenInclude(d => d.AppUser).ToListAsync();
 		}
 
-		public async Task<CategoryPost> GetDepartmentPostByIncludeAsync(int id)
+		public async Task<CategoryPost> GetCategoryPostByIncludeAsync(int id)
 		{
 			return await _context.CategoryPosts.Include(d => d.Category).Include(d => d.Post).ThenInclude(d => d.PostComments).ThenInclude(d => d.AppUser).FirstOrDefaultAsync(p => p.Id == id);
 		}
 
-		public async Task<List<CategoryPost>> GetSomeDepartmentPostByIncludeAsync(Expression<Func<CategoryPost, bool>> expression)
+		public async Task<List<CategoryPost>> GetSomeCategoryPostByIncludeAsync(Expression<Func<CategoryPost, bool>> expression)
 		{
 			return await _context.CategoryPosts.Where(expression).Include(d => d.Category).Include(d => d.Post).ThenInclude(d => d.PostComments).ThenInclude(d => d.AppUser).ToListAsync();
 		}
